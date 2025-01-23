44e48acd527db3c46462b5f6ac44178e0450eb71
sudhanshu130199 <sudhanshu1313@gmail.com>
2025-01-23 11:30:52 +0530

    github action

---
+++ test_square.py
@@ -0,0 +1,8 @@
+from square import square_num
+
+def test_square_num():
+    a = 6
+    res = square_num(a)
+    assert res == 36
+
+#  Setup everything i.e.  bring functions to test --> Execute the test for these function --> verify the results
