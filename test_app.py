45a7832a05849d49f90fddcf8d31b3d13bf8e4a6
sudhanshu130199 <sudhanshu1313@gmail.com>
2025-01-23 23:01:35 +0530

    action

--- test_app.py
+++ test_app.py
@@ -17,6 +17,6 @@ def test_aboutus(client):
 def test_predict(client):
     test_data={'Gender':"Male", 'Married':"Unmarried",'Credit_History' : "Unclear Debts",'ApplicantIncome':100000,'LoanAmount':2000000}
     resp=client.post('/prediction', json=test_data)
-    assert resp.status_code == 200
+    assert resp.status_code == 404
     assert resp.json=={'loan_approval_status': 'Rejected'}
 
